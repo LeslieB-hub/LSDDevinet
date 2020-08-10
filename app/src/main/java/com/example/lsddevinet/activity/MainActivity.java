37d61bc87bf6d5311bcdd724081131e0f6c01536
Leslie <leslie.bileckot@gmail.com>
2020-08-10 15:16:17 +0200

    add dependencies to gradle
    add stheto method to mainActivity

--- app/src/main/java/com/example/lsddevinet/activity/MainActivity.java
+++ app/src/main/java/com/example/lsddevinet/activity/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.lsddevinet.R;
+import com.facebook.stetho.Stetho;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Stetho.initializeWithDefaults(this);
         setContentView(R.layout.activity_main);
     }
 }
