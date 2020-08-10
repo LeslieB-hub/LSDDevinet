ce563e55f6dcdb0bfdc238cfac417743fc125508
Leslie <leslie.bileckot@gmail.com>
2020-08-10 15:48:27 +0200

    Create menu.xml
    add menu to the mainActivity

--- app/src/main/java/com/example/lsddevinet/activity/MainActivity.java
+++ app/src/main/java/com/example/lsddevinet/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.lsddevinet.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.lsddevinet.R;
 import com.facebook.stetho.Stetho;
@@ -14,5 +18,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Stetho.initializeWithDefaults(this);
         setContentView(R.layout.activity_main);
+
     }
+
+    /**
+     * Menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //on décompresse le xml du menu
+        getMenuInflater().inflate(R.menu.mon_menu, menu);
+        return true;
+    }
+
+//    /**
+//     * Définition des actions
+//     * @param item
+//     * @return
+//     */
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+//        switch (item.getItemId()){
+//            case R.id.action_Accueil:
+//                Intent intentMain = new Intent(this, MainActivity.class);
+//                startActivity(intentMain);
+//                return true;
+//            case R.id.action_settings:
+//                Intent intentParametre = new Intent(this, .class);
+//                startActivity(intentParametre);
+//                return true;
+//            case R.id.action_APropos:
+//                Intent intentAPropos = new Intent(this, .class);
+//                startActivity(intentAPropos);
+//                return true;
+//            case R.id.action_PagePrecedente:
+//
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//    }
+
+
 }
