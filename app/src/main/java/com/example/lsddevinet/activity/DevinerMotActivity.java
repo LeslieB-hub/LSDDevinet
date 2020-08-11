50a68db2dea7f7da9dea06aea7020128477f3983
Dominika <dominika.sliwa@gmail.com>
2020-08-11 12:09:39 +0200

    petites modif manifest

--- app/src/main/java/com/example/lsddevinet/activity/DevinerMotActivity.java
+++ app/src/main/java/com/example/lsddevinet/activity/DevinerMotActivity.java
@@ -2,7 +2,9 @@ package com.example.lsddevinet.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.lsddevinet.R;
 
@@ -12,5 +14,21 @@ public class DevinerMotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deviner_mot);
+
+        Intent intent = getIntent();
+    }
+
+
+
+    /**
+     * Menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //on décompresse le xml du menu
+        getMenuInflater().inflate(R.menu.mon_menu, menu);
+        return true;
     }
 }
