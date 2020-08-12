279cde3f43f1c158f46ec2693582a7c27c236978
Leslie <leslie.bileckot@gmail.com>
2020-08-12 09:29:41 +0200

    modify id catagorie

--- app/src/main/java/com/example/lsddevinet/dal/AppDatabase.java
+++ app/src/main/java/com/example/lsddevinet/dal/AppDatabase.java
@@ -45,54 +45,54 @@ public abstract class AppDatabase extends RoomDatabase{
                 protected Void doInBackground(AppDatabase... appDatabases) {
                     MotDao motDao = INSTANCE_CNX.getMotDao();
                     CategorieDao categorieDao = INSTANCE_CNX.getCategorieDao();
-                    categorieDao.insert(new Categorie(4,"4 lettres"));
-                    categorieDao.insert(new Categorie(5,"5 lettres"));
-                    categorieDao.insert(new Categorie(6,"6 lettres"));
-                    categorieDao.insert(new Categorie(7,"7 lettres"));
-                    categorieDao.insert(new Categorie(8,"8 lettres"));
-                    categorieDao.insert(new Categorie(9,"9 lettres"));
-                    categorieDao.insert(new Categorie(10,"10 lettres"));
+                    categorieDao.insert(new Categorie(1,"4 lettres"));
+                    categorieDao.insert(new Categorie(2,"5 lettres"));
+                    categorieDao.insert(new Categorie(3,"6 lettres"));
+                    categorieDao.insert(new Categorie(4,"7 lettres"));
+                    categorieDao.insert(new Categorie(5,"8 lettres"));
+                    categorieDao.insert(new Categorie(6,"9 lettres"));
+                    categorieDao.insert(new Categorie(7,"10 lettres"));
 
-                    motDao.insert(new Mot(0, "kiwi.jpeg", "kiwi", "", 4 ));
-                    motDao.insert(new Mot(0, "tank.jpeg", "tank", "tank", 4 ));
-                    motDao.insert(new Mot(0, "chat.jpeg", "chat", "", 4 ));
-                    motDao.insert(new Mot(0, "main.jpeg", "main", "", 4 ));
-                    motDao.insert(new Mot(0, "pain.jpeg", "pain", "pain", 4 ));
-                    motDao.insert(new Mot(0, "oeil.jpeg", "oeil", "oeil", 4 ));
+                    motDao.insert(new Mot(0, "kiwi.jpeg", "kiwi", "", 1 ));
+                    motDao.insert(new Mot(0, "tank.jpeg", "tank", "tank", 1 ));
+                    motDao.insert(new Mot(0, "chat.jpeg", "chat", "", 1 ));
+                    motDao.insert(new Mot(0, "main.jpeg", "main", "", 1 ));
+                    motDao.insert(new Mot(0, "pain.jpeg", "pain", "pain", 1 ));
+                    motDao.insert(new Mot(0, "oeil.jpeg", "oeil", "oeil", 1 ));
 
-                    motDao.insert(new Mot(0, "chien.jpeg", "chien", "", 5 ));
-                    motDao.insert(new Mot(0, "pomme.jpeg", "pomme", "", 5 ));
-                    motDao.insert(new Mot(0, "raisin.jpeg", "raisin", "", 5 ));
-                    motDao.insert(new Mot(0, "stylo.jpeg", "stylo", "", 5 ));
-                    motDao.insert(new Mot(0, "herbe.jpeg", "herbe", "", 5 ));
-                    motDao.insert(new Mot(0, "livre.jpeg", "livre", "livre", 5 ));
+                    motDao.insert(new Mot(0, "chien.jpeg", "chien", "", 2 ));
+                    motDao.insert(new Mot(0, "pomme.jpeg", "pomme", "", 2 ));
+                    motDao.insert(new Mot(0, "raisin.jpeg", "raisin", "", 2 ));
+                    motDao.insert(new Mot(0, "stylo.jpeg", "stylo", "", 2 ));
+                    motDao.insert(new Mot(0, "herbe.jpeg", "herbe", "", 2 ));
+                    motDao.insert(new Mot(0, "livre.jpeg", "livre", "livre", 2 ));
 
-                    motDao.insert(new Mot(0, "chaise.jpeg", "chaise", "chaise", 6 ));
-                    motDao.insert(new Mot(0, "dragon.jpeg", "dragon", "dragon", 6 ));
-                    motDao.insert(new Mot(0, "soleil.jpeg", "soleil", "soleil", 6 ));
-                    motDao.insert(new Mot(0, "fraise.jpeg", "fraise", "", 6 ));
-                    motDao.insert(new Mot(0, "miroir.jpeg", "miroir", "", 6 ));
-                    motDao.insert(new Mot(0, "bateau.jpeg", "bateau", "", 6 ));
+                    motDao.insert(new Mot(0, "chaise.jpeg", "chaise", "chaise", 3));
+                    motDao.insert(new Mot(0, "dragon.jpeg", "dragon", "dragon", 3 ));
+                    motDao.insert(new Mot(0, "soleil.jpeg", "soleil", "soleil", 3 ));
+                    motDao.insert(new Mot(0, "fraise.jpeg", "fraise", "", 3 ));
+                    motDao.insert(new Mot(0, "miroir.jpeg", "miroir", "", 3 ));
+                    motDao.insert(new Mot(0, "bateau.jpeg", "bateau", "", 3 ));
 
-                    motDao.insert(new Mot(0, "abricot.jpeg", "abricot", "", 7 ));
-                    motDao.insert(new Mot(0, "abeille.jpeg", "abeille", "", 7 ));
-                    motDao.insert(new Mot(0, "ananas.jpeg", "ananas", "", 7 ));
-                    motDao.insert(new Mot(0, "poisson.jpeg", "poisson", "", 7 ));
-                    motDao.insert(new Mot(0, "carotte.jpeg", "carotte", "", 7 ));
+                    motDao.insert(new Mot(0, "abricot.jpeg", "abricot", "", 4 ));
+                    motDao.insert(new Mot(0, "abeille.jpeg", "abeille", "", 4 ));
+                    motDao.insert(new Mot(0, "ananas.jpeg", "ananas", "", 4 ));
+                    motDao.insert(new Mot(0, "poisson.jpeg", "poisson", "", 4 ));
+                    motDao.insert(new Mot(0, "carotte.jpeg", "carotte", "", 4 ));
 
-                    motDao.insert(new Mot(0, "couronne.jpeg", "couronne", "", 8 ));
-                    motDao.insert(new Mot(0, "pastèque.jpeg", "pastèque", "", 8 ));
-                    motDao.insert(new Mot(0, "papillon.jpeg", "papillon", "", 8 ));
+                    motDao.insert(new Mot(0, "couronne.jpeg", "couronne", "", 5 ));
+                    motDao.insert(new Mot(0, "pastèque.jpeg", "pastèque", "", 5 ));
+                    motDao.insert(new Mot(0, "papillon.jpeg", "papillon", "", 5 ));
 
-                    motDao.insert(new Mot(0, "enveloppe.jpeg", "enveloppe", "", 9 ));
-                    motDao.insert(new Mot(0, "coccinelle.jpeg", "coccinelle", "", 9 ));
-                    motDao.insert(new Mot(0, "chaussure.jpeg", "chaussure", "", 9 ));
-                    motDao.insert(new Mot(0, "autoroute.jpeg", "autoroute", "", 9 ));
-                    motDao.insert(new Mot(0, "cigarette.jpeg", "cigarette", "", 9 ));
+                    motDao.insert(new Mot(0, "enveloppe.jpeg", "enveloppe", "", 6 ));
+                    motDao.insert(new Mot(0, "coccinelle.jpeg", "coccinelle", "", 6 ));
+                    motDao.insert(new Mot(0, "chaussure.jpeg", "chaussure", "", 6 ));
+                    motDao.insert(new Mot(0, "autoroute.jpeg", "autoroute", "", 6 ));
+                    motDao.insert(new Mot(0, "cigarette.jpeg", "cigarette", "", 6 ));
 
 
-                    motDao.insert(new Mot(0, "grenouille.jpeg", "grenouille", "grenouille", 10 ));
-                    motDao.insert(new Mot(0, "champignon.jpeg", "champignon", "champignon", 10 ));
+                    motDao.insert(new Mot(0, "grenouille.jpeg", "grenouille", "grenouille", 7 ));
+                    motDao.insert(new Mot(0, "champignon.jpeg", "champignon", "champignon", 7 ));
 
                     return null;
                 }
