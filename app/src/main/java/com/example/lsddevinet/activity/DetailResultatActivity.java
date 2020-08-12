2862a71b2146a6c2337979401399ca2fb56f3072
Leslie <leslie.bileckot@gmail.com>
2020-08-12 17:24:43 +0200

    modify package adapterdetailresultat

--- app/src/main/java/com/example/lsddevinet/activity/DetailResultatActivity.java
+++ app/src/main/java/com/example/lsddevinet/activity/DetailResultatActivity.java
@@ -1,14 +1,99 @@
-package com.example.lsddevinet;
+package com.example.lsddevinet.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.lsddevinet.R;
+import com.example.lsddevinet.ViewModel.MotViewModel;
+import com.example.lsddevinet.activity.adapters.AdapterResultatDetail;
+import com.example.lsddevinet.model.Categorie;
+import com.example.lsddevinet.model.Mot;
+
+import java.util.List;
 
 public class DetailResultatActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_resultat);
+
+        //récupérer le recycleview
+        recyclerView = findViewById(R.id.recyclerview_resultat);
+        //optimise le chargement quand le rv ne change pas de taille
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager rvlm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(rvlm);
+
+        //Récupérer la liste entière de mots
+        MotViewModel motVM = ViewModelProviders.of(this).get(MotViewModel.class);
+
+        //LiveData<List<Mot>> observateur = motVM.getAllMots();
+        //Log.i("Devinet", "liste de mots " + observateur.getValue().get(0));
+
+
+        motVM.getAllMots().observe(this, new Observer<List<Mot>>() {
+            @Override
+            public void onChanged(List<Mot> mots) {
+               // Log.i("Devinet", "liste de mots " + mots.get(0));
+                AdapterResultatDetail adapterResultatDetail = new AdapterResultatDetail(mots);
+                recyclerView.setAdapter(adapterResultatDetail);
+            }
+
+        });
+    }
+
+    /**
+     * Menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //on décompresse le xml du menu
+        getMenuInflater().inflate(R.menu.mon_menu, menu);
+        return true;
+    }
+
+    /**
+     * Définition des actions
+     * @param item
+     * @return
+     */
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_Accueil:
+                Intent intentMain = new Intent(this, MainActivity.class);
+                startActivity(intentMain);
+                return true;
+            case R.id.action_settings:
+                Intent intentParametre = new Intent(this, ParametreActivity.class);
+                startActivity(intentParametre);
+                return true;
+            case R.id.action_APropos:
+                Intent intentAPropos = new Intent(this, AProposActivity.class);
+                startActivity(intentAPropos);
+                return true;
+            case R.id.action_PagePrecedente:
+//                Intent intentResultat = new Intent(this, MainActivity.class);
+//                startActivity(intentResultat);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
--- app/src/main/java/com/example/lsddevinet/activity/adapters/AdapterResultatDetail.java
+++ app/src/main/java/com/example/lsddevinet/activity/adapters/AdapterResultatDetail.java
@@ -1,4 +1,72 @@
 package com.example.lsddevinet.activity.adapters;
 
-public class AdapterResultatDetail {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lsddevinet.R;
+import com.example.lsddevinet.model.Mot;
+
+import java.util.List;
+
+public class AdapterResultatDetail extends RecyclerView.Adapter<AdapterResultatDetail.ViewHolder> {
+
+    private List<Mot> mots;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageView;
+        public EditText editTextMot;
+        public EditText editTextMotPropose;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image);
+            editTextMot = itemView.findViewById(R.id.ed_mot);
+            editTextMotPropose = itemView.findViewById(R.id.ed_motpropose);
+        }
+
+    }
+
+    /**
+     * Constructeur de l'adapter
+     * @param mesMots
+     */
+    public AdapterResultatDetail(List<Mot> mesMots){
+            mots = mesMots;
+    }
+
+
+    /**
+     * Création de la view
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public AdapterResultatDetail.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater. from (parent.getContext()).inflate(R.layout.activity_detail_resultat , parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterResultatDetail.ViewHolder holder, int position) {
+        //holder.imageView.setImageURI(mots.get(position).getImg()); bo img string  ou mettre l'image ds onCreate
+        holder.editTextMot.setText(mots.get(position).getMot());
+        holder.editTextMotPropose.setText(mots.get(position).getProposition());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mots.size();
+    }
+
 }
